b4833ca5fd1f8b8a6b8cb1730a3ae477084d6384
John Cyrill Corsanes <johncyril@gorated.ph>
2019-10-21 18:42:30 +0800

    Updated Plyr to version 3.5.4

---
+++ assets/loader.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const plyr_player = new Plyr('#plyr-player');
+    window.player = plyr_player;
+});
--- simple-plyr.php
+++ simple-plyr.php
@@ -1,56 +1,80 @@
 <?php
 /**
  *
- * @link              https://github.com/bestony/simple-plyr
- * @since             0.0.1
- * @package           Simple Plyr
+ * @link              https://github.com/jcchikikomori/simple-plyr
+ * @since             0.0.2
+ * @package           Simple Plyr (PHP7)
  *
  * @wordpress-plugin
  * Plugin Name:       Simple Plyr
- * Plugin URI:        https://github.com/bestony/simple-plyr
+ * Plugin URI:        https://github.com/jcchikikomori/simple-plyr
  * Description:       Simple Plyr Video Player
- * Version:           0.0.1
+ * Version:           0.0.2
  * Author:            Bestony
- * Author URI:        https://github.com/bestony
+ * Author URI:        https://github.com/jcchikikomori
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plyrio
  * Domain Path:       /languages
  */
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-    die;
-}
+if ( ! defined( 'WPINC' ) ) { die; }
 
-// Add Shortcode
+use RicardoFiorani\Matcher\VideoServiceMatcher;
+require __DIR__ . '/vendor/autoload.php';
+
+/**
+ * Add Shortcode
+ * @param $atts
+ *
+ * @return string
+ * @throws \RicardoFiorani\Exception\ServiceNotAvailableException
+ */
 function plyr_filter( $atts ) {
 
     // Attributes
     $atts = shortcode_atts(
         array(
-            'url' => '/path/to/video.mp4',
+            'url' => 'https://www.youtube.com/watch?v=gM0qOa_H-rs', // (c) Nikon Europe. C.C.
             'poster' => '/path/to/poster.jpg',
         ),
         $atts,
         'plyr'
     );
 
-    $str = sprintf("<video poster='%s' controls><source src='%s' type='video/mp4'></video><script>plyr.setup();</script>",$atts['poster'],$atts['url']);
+    $vsm = new VideoServiceMatcher();
+
+    // Detects which service the url belongs to and returns the service's implementation of RicardoFiorani\Adapter\VideoAdapterInterface
+    $video = $vsm->parse($atts['url']);
+
+    // if video was from YouTube or Vimeo
+    if ($video instanceof RicardoFiorani\Adapter\Youtube\YoutubeServiceAdapter ||
+        $video instanceof RicardoFiorani\Adapter\Vimeo\VimeoServiceAdapter ) {
+        $str = sprintf("<div class='plyr__video-embed plyr-embed' id='plyr-player' poster='%s'>
+                                   <iframe src='%s' allowfullscreen allowtransparency allow=\"autoplay\"></iframe>
+                               </div>", $atts['poster'], $atts['url']);
+    }
+    // use HTML5 video code
+    else {
+        $str = sprintf("<video id='plyr-player' poster='%s' controls><source src='%s' type='video/mp4'></video>",$atts['poster'], $atts['url']);
+    }
 
     return $str;
 }
-add_shortcode( 'plyr', 'plyr_filter' );
+
 
 function plyr_assets() {
     $plugin_url = plugin_dir_url( __FILE__ );
-    wp_register_style( 'plyr-style',$plugin_url . 'assets/plyr.css');
-    wp_enqueue_style( 'plyr-style' );
 
-    wp_register_script ( 'plyr-script',$plugin_url . 'assets/plyr.js' );
-    wp_enqueue_script ( 'plyr-script' );
+    wp_register_style('plyr-style', $plugin_url . 'assets/plyr.css');
+    wp_enqueue_style('plyr-style');
+
+    wp_register_script('plyr-script', $plugin_url . 'assets/plyr.min.js');
+    wp_enqueue_script('plyr-script');
+
+    wp_register_script('plyr-loader-script', $plugin_url . 'assets/loader.js');
+    wp_enqueue_script('plyr-loader-script');
 }
-add_action( 'wp_enqueue_scripts', 'plyr_assets' );
-
 
 // Add Quicktags
 function plyr_quicktags() {
@@ -64,4 +88,7 @@ function plyr_quicktags() {
     }
 
 }
+
+add_shortcode( 'plyr', 'plyr_filter' );
+add_action( 'wp_enqueue_scripts', 'plyr_assets' );
 add_action( 'admin_print_footer_scripts', 'plyr_quicktags' );
